fcd36f596c7a2fda40789687ca9ecd088771c05c
leandreonizuka <125875013+leandreonizuka@users.noreply.github.com>
2023-02-22 09:34:52 +0100

    Add files via upload

---
+++ base64tool.py
@@ -0,0 +1,28 @@
+import base64 
+
+def encode_base64():
+	message = input("[*]enter what you want to encode : ")
+	ascii_msg = message.encode('ascii')
+	bytes = base64.b64encode(ascii_msg)
+	msg_base64 = bytes.decode('ascii')
+	print("[*]",msg_base64)
+
+def decode_base64():
+	message = input("[*]enter what you what de decode :")
+	encode_msg = message.encode('ascii')
+	bytes = base64.b64decode(encode_msg)
+	final_msg = bytes.decode('ascii')
+	print("[*]",final_msg)
+
+print("#base64 decode = a")
+print("#base64 encode = b")  
+
+c = input("enter what you whant :  ")
+
+if c == "a":
+	decode_base64()
+elif c == "b": 
+	encode_base64()
+
+else:
+	print ("***bad enter***")
